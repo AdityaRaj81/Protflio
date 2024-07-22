e684639ea8437e54d6ef41b53a9c61ec5f68ac01
Mr. Aditya <Connect2RajAditya@gmail.com>
2024-07-22 05:02:26 +0000

    app.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Adity Raj",
+  title: "Web Designer",
+  email: "connect2RahAditya@gmail.com",
+  gitHub: "adityaRaj81",
+  instagram: "adityaRaj_81",
+  linkedIn: "adityaraj81",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "adityaraj_81",
+  facebook: "adityaraj_812",
 };
 
 const primaryColor = "#4E567E";
